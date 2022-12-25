08c8874b8f7066e9152337aba7a251bdf4f19156
mosguinz <mos.guinz@gmail.com>
2022-12-25 15:17:02 -0800

    Add guard to prevent script from running on other sites

--- bookmark.js
+++ bookmark.js
@@ -1,4 +1,10 @@
 javascript: (() => {
+
+    if (window.location.hostname != "widget.transitapp.com") {
+        return window.confirm("Script meant to be run on widget.transitapp.com only.\nOpen widget.transitapp.com?")
+            && window.open("https://widget.transitapp.com");
+    }
+
     let routes = document.getElementById("routes");
     let clickOnRoutes = () => {
         for (let route of routes.children) {
