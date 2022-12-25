86aafc6b3d10abea3d1e7a304cef67f156a9ff32
mosguinz <mos.guinz@gmail.com>
2022-12-25 15:31:35 -0800

    Prevent script from running more than once

--- bookmark.js
+++ bookmark.js
@@ -1,9 +1,13 @@
 javascript: (() => {
-
     if (window.location.hostname != "widget.transitapp.com") {
         return window.confirm("Script meant to be run on widget.transitapp.com only.\nOpen widget.transitapp.com?")
             && window.open("https://widget.transitapp.com");
     }
+
+    if (window.transitDisplayTriggered) {
+        return;
+    }
+    window.transitDisplayTriggered = true;
 
     let routes = document.getElementById("routes");
     let clickOnRoutes = () => {
